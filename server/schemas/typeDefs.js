146c13c9a7bb558e8abbeb44afe074e8162ae9ce
Sumit Ojha <sumitojha501@gmail.com>
2022-10-20 18:08:04 -0500

    Updated schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,19 @@ const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
   type Book {
     bookId: ID!
     authors: [String]
@@ -11,9 +24,20 @@ const typeDefs = gql`
     link: String
     title: String!
   }
-
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
   type Query {
-    books: [Book]
+    me: User
+  }
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
