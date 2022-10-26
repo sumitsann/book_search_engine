43dfe9d807896e495cbc0b6f858be19ca333aa7f
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-10-26 08:34:39 -0500

    Updated connection file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/search-books",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/search-books",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
