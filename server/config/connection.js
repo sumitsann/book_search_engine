ca1d9e5b13e657243b6275a88e747aabab1f42be
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-10-26 08:28:18 -0500

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/book-search-db",
+  process.env.MONGODB_URI || "mongodb://localhost/search-books",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
   }
 );
 
